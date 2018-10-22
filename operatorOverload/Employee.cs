97ca7da19ced3ea6b4c6cba31669427366f0746a
Ryan <werner.ryan18@gmail.com>
2018-10-22 12:08:25 -0700

    Add project files.

---
+++ operatorOverload/Employee.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace operatorOverload
+{
+    public class Employee
+    {
+        int id;
+        public Employee()
+        {
+            id = 0;
+        }
+        public Employee(int x)
+        {
+            id = x;
+        }
+
+        public static bool operator == (Employee x, Employee y)
+        {
+            if (x.id == y.id) { return true; }
+            else
+                return false;
+        }
+        public static bool operator != (Employee x, Employee y)
+        {
+            if(x.id != y.id) { return true; }
+            else
+                return false;
+        }
+        public int getValue()
+        {
+            return id;
+        }
+    }
+}
---
+++ operatorOverload/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace operatorOverload
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int x, y;
+            string holder;
+
+            Console.WriteLine("Enter the first employee's ID: ");
+            holder = Console.ReadLine();
+            x = Convert.ToInt32(holder);
+
+            Console.WriteLine("Enter the second employee's ID: ");
+            holder = Console.ReadLine();
+            y = Convert.ToInt32(holder);
+
+
+            Employee idX = new Employee(x);
+            Employee idY = new Employee(y);
+
+            if(idX==idY)
+            {
+                Console.WriteLine("Equal");
+            }
+            else
+            {
+                Console.WriteLine("Not Equal");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
